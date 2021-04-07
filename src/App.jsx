7f5f64f2d4acc46629730ba2f5140e9ab4aa1936
Maxim Dolzhenkov <maksim-do@yandex.ru>
2021-04-07 16:23:19 +0500

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,132 @@
 import {
   AppBar,
-  Box,
   Typography,
   Container,
   Toolbar,
   IconButton,
+  Box,
+  Button,
 } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
+//  import Button from '@material-ui/core/Button';
 import React from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+const headersData = [
+  {
+    label: 'Университеты',
+    href: '/universities',
+  },
+  {
+    label: 'Специальности',
+    href: '/specializations',
+  },
+  {
+    label: 'Профессии',
+    href: '/professions',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
+  root: {},
+  container: {
+    backgroundColor: '#FFFFFF',
+    padding: 0,
   },
   menuButten: {
-    marginRight: theme.spacing(2),
+    color: '#023047',
+    [theme.breakpoints.up('tablet')]: {
+      display: 'none',
+    },
   },
-  title: {
+  menuButtenIcon: {
+    fontSize: '32px',
+  },
+  fill: {
     flexGrow: 1,
   },
+  title: {
+    [theme.breakpoints.up('tablet')]: {
+      marginLeft: theme.spacing(2),
+    },
+    [theme.breakpoints.up('desktop')]: {
+      fontSize: '27px',
+    },
+  },
+  navButton: {
+    color: '#023047',
+    fontStyle: 'normal',
+    fontSize: '12px',
+    marginTop: '5px',
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.up('desktop')]: {
+      fontSize: '14px',
+      marginTop: '8px',
+    },
+  },
+
+  toolBar: {},
+
+  nav: {
+    marginLeft: theme.spacing(10),
+  },
+
+  header: {
+    boxShadow: 'none',
+  },
 }));
+
+function Nav() {
+  const classes = useStyles();
+  const ListNav = headersData.map(({ label, href }) => (
+    <Button
+      className={classes.navButton}
+      {...{ key: label, color: 'inherit', to: href }}
+    >
+      {label}
+    </Button>
+  ));
+
+  return (
+    <Box
+      display={{ mobile: 'none', tablet: 'block', desktop: 'block' }}
+      className={classes.nav}
+    >
+      {ListNav}
+    </Box>
+  );
+}
+
 function App() {
   const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.up('tablet'));
   return (
-    <AppBar position="fixed">
-      <Container fixed>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            area-label="menu"
-            className={classes.menuButten}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6">
-            Web site witch Materiel-UI
-          </Typography>
-          <Box mr={3}>
-            <Button color="inherit" variant="outlined">
-              Log In
-            </Button>
-          </Box>
-          <Button color="secondary" variant="contained">
-            Sign Up
-          </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <>
+      <AppBar className={classes.header} position="fixed">
+        <Container className={classes.container} fixed={isMobile}>
+          <Toolbar className={classes.toolBar}>
+            <Typography className={classes.title} variant="h6">
+              Унивитрина
+            </Typography>
+            <Box
+              className={classes.fill}
+              display={{ mobile: 'block', tablet: 'none', desktop: 'none' }}
+            />
+            <Nav />
+            <IconButton
+              edge="start"
+              color="inherit"
+              area-label="menu"
+              className={classes.menuButten}
+            >
+              <MenuIcon className={classes.menuButtenIcon} />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const BREAKPOINTS = {
+  mobile: 320,
+  tablet: 768,
+  desktop: 1000,
+};
+
+const breakpointsFull = {
+  breakpoints: createBreakpoints({
+    values: BREAKPOINTS,
+  }),
+};
+const myTheme = {
+  typography: {
+    h6: {
+      color: '#023047',
+    },
+    body1: {
+      fontWeight: 500,
+    },
+    button: {
+      fontStyle: 'italic',
+    },
+  },
+};
+const thememy = createMuiTheme(myTheme, breakpointsFull);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MuiThemeProvider theme={thememy}>
+      <App />
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
